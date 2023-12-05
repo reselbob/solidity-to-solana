970866b0d411659d7878b353aebcb4ed455fe204
Bob Reselman <bob@CogArtTech.com>
2023-12-05 10:26:09 -0800

    Update Simple.sol

--- solidity/Simple.sol
+++ solidity/Simple.sol
@@ -1,1 +1,17 @@
-// code
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Simple {
+    address public owner;
+
+    // Constructor to set the contract deployer as the owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    // Function to check if the sender is the owner
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
+    }
+}
