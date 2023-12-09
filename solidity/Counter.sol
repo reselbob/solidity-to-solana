0d574045d899d7954f9d4135f2029cb999679bea
reselbob <bob@CogArtTech.com>
2023-12-09 10:13:42 -0800

    updating contracts

---
+++ solidity/Counter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract counter {
+    // The counter value that is stored in the account
+    uint64 private count;
+
+    // The constructor is used to create a new counter accounton
+    constructor() {
+        // Initialize the count to zero
+        count = 0;
+    }
+
+    // Increments the count by one.
+    function increment() public {
+        count += 1;
+    }
+
+    // Returns the count value
+    function get() public view returns (uint64) {
+        return count;
+    }
+}
--- solidity/SayHiContract.sol
+++ solidity/SayHiContract.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract SayHiContract {
-    // Function to get the message "Hi"
     function getMessage() public pure returns (string memory) {
         return "Hi from Solana that used to be Solidity code!";
     }
