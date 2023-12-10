4398af8e1841587c0fe6c2ddad7fd1c721bc8437
reselbob <bob@CogArtTech.com>
2023-12-10 13:31:41 -0800

    removing redundant code

--- solexplorer/SayHiClient.ts
+++ solexplorer/SayHiClient.ts
@@ -5,8 +5,6 @@ console.log("My address:", pg.wallet.publicKey.toString());
 const balance = await pg.connection.getBalance(pg.wallet.publicKey);
 console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
 
-const testKp = new web3.Keypair();
-
 async function callGetMessage() {
   // get the metrics on the program
   const accountInfo = await pg.connection.getAccountInfo(pg.PROGRAM_ID);
